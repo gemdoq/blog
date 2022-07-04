9f4a0cb4920a1e205b927808159f0013e81d9000
gemdoq <gemdoq@gmail.com>
2022-07-04 18:14:54 +0900

    models user,post,category,comment

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/');
+app.get("/");
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import app from './app.js';
+import app from "./app.js";
 
-app.listen('3000', () => {
-    console.log('hi');
+app.listen("3000", () => {
+    console.log("hi");
 });
